765f7bab20f36687208b54687ce6d345ff6f7a1d
Hannan <sodrunberkicau@gmail.com>
2025-01-23 22:49:24 +0800

    add change notifier provider to runApp function in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:word_explorer/viewmodels/word_viewmodel.dart';
+
 import 'package:word_explorer/word_explorer_app.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const WordExplorerApp());
+  runApp(
+    ChangeNotifierProvider<WordViewModel>(
+      create: (context) => WordViewModel(),
+      child: const WordExplorerApp(),
+    ),
+  );
 }
